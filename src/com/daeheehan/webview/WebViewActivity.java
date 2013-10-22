4162ff79ee89297af5942d60d68ed9baae58250a
daehehan <bookdori81@gmail.com>
2013-10-22 16:39:54 -0700

    Applying RoboGuice Library

--- src/com/daeheehan/webview/WebViewActivity.java
+++ src/com/daeheehan/webview/WebViewActivity.java
@@ -1,29 +1,26 @@
 package com.daeheehan.webview;
 
-import android.app.Activity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.webkit.WebView;
-import android.widget.Toast;
 
-public class WebViewActivity extends Activity {
+public class WebViewActivity extends RoboActivity {
 	private static final String CNAME = MainActivity.class.getSimpleName();
 
-	private WebView mWebView;
+	@InjectView(R.id.webView) private WebView mWebView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_webview);
-		
-		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		mWebView = (WebView) findViewById(R.id.webView);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		String rUrl = "https://github.com/booknara/WebViewExample";
 		mWebView.loadUrl(rUrl);
+		
 	}
 	
 	@Override
